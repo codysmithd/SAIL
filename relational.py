43631355622d129b88ab15f8d9aeda3a9d3601cf
Cody Smith <codysmithd@gmail.com>
2014-04-28 17:30:23 -0400

    Added get_top_links to node

--- relational.py
+++ relational.py
@@ -6,27 +6,54 @@ File Format:
 
 # Class that contains a Word, Part Of Speech (POS), and of hash table of weighted sums to other words
 class node:
+
     def __init__(self, word = "", pos = "", links = {}):
         self.word = word.lower()
         self.pos = pos.lower()
         self.links = links
 
+    # Returns true if node is not null (empty)
     def __nonzero__(self):
         return self.word and self.pos 
 
+    # Either adds weight to existing link or makes a new link
     def link(self, node, weight = 1):
-        if(node.word in self.links):
-            self.links[node.word] += 1
+        hash_index = node.word + " " + node.pos
+        if(hash_index in self.links):
+            self.links[hash_index] += 1
         else:
-            self.links[node] = weight
+            self.links[hash_index] = weight
 
+    # Returns a list of size n of the top links in-order, optionally given a Part Of Speech
+    def get_top_links(self, n, pos = ""):
+        top_links = []
+        for key in self.links:
+            if(key.split()[1] == pos or pos == ""):
+                word = key.split()[0]
+                value = self.links[key]
+                if(len(top_links) > 1):
+                    for x in xrange(0,len(top_links)):
+                        if(value < top_links[x][1]):
+                            top_links.insert(x,(word,value))
+                else:
+                    top_links.append( (word,value) )
+        final_list = []
+        for x in xrange(0,len(top_links)):
+            if x < n:
+                final_list.append(top_links[x][0])
+            else:
+                break
+        return final_list
+
+    # Returns a string formatted for output
     def print_out(self):
         buffer = "" + self.word + ' ' + self.pos + '\n'
         for key in self.links:
-            buffer += key + ' ' + self.links + ' '
+            buffer += key + ' ' + self.links[key] + ' '
         buffer += '\n'
         return buffer
 
+    # Builds node based on string (assuming proper format)
     def read_in(self, string):
         lines = string.splitlines()
         self.word = lines[0].split()[0]
@@ -39,12 +66,13 @@ class node:
             self.link(links[x],links[x+1])
 
 
-# Class that contains a list of nodes that link to each other with weights
+# Class that contains a list of nodes that link to each other with weights.
 class relational_map:
 
     def __init__(self):
-        self.node_list = {}
+        self.node_hash = {}
 
+    # Loads in a relational map from text file
     def load_from_file(self, filename):
         f = open(filename, 'r')
         if(f):
@@ -52,12 +80,20 @@ class relational_map:
             for x in xrange(0,len(lines),2):
                 new_node = node()
                 new_node.read_in(lines[x] + lines[x+1])
-                node_list[new_node.word] = new_node
+                node_hash[new_node.word] = new_node
             f.close()
 
+    # Outputs the relational map to a file with the given filename as text
     def output_file(self, filename):
         f = open(filename, 'w')
         if(f):
-            for key in self.node_list:
-                f.write(self.node_list[key].print_out())
+            for key in self.node_hash:
+                f.write(self.node_hash[key].print_out())
             f.close()
+
+    # Links the words. Words is a list of strings, formatted "<word> <part_of_speech>"
+    def link_words(self, words):
+        for word in words:
+            if(not word in self.node_hash):
+                pass
+
