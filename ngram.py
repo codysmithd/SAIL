7b2b8778d0bbcda7d04c4416ccb6b3a9535a9032
Brendan Whitfield <brendanw@windworksdesign.com>
2014-05-04 21:08:36 -0400

    moved ngram functions back to seperate file

---
+++ ngram.py
@@ -0,0 +1,59 @@
+
+
+import json
+
+
+ngram_backward = None
+ngram_forward = None
+
+
+def load():
+	global ngram_backward
+	global ngram_forward
+
+	print "1 of 2"
+	f = open("datasets/ngram_backward.json", 'r')
+	ngram_backward = json.load(f)
+	f.close()
+
+	print "2 of 2"
+	f = open("datasets/ngram_forward.json", 'r')
+	ngram_forward = json.load(f)
+	f.close()
+
+
+def rateSentence(words):
+	global ngram_backward
+
+	rating = 0
+	prev1 = "<s>"
+	prev2 = "<s>"
+
+	# backward ngrams
+	for index, word in enumerate(words):
+		if index >= 1:
+			if ngram_backward.has_key(word):
+				if ngram_backward[word].has_key(prev1):
+					if ngram_backward[word][prev1].has_key(prev2):
+						rating += ngram_backward[word][prev1][prev2][0]
+		prev2 = prev1
+		prev1 = word
+	return rating
+
+
+
+def getNextWords(word):
+	global ngram_forward
+
+	if ngram_forward.has_key(word):
+		return ngram_forward[word].keys()
+	else:
+		return None
+
+def getPrevWords(word):
+	global ngram_backward
+
+	if ngram_backward.has_key(word):
+		return ngram_backward[word].keys()
+	else:
+		return None
--- sentence_assembler.py
+++ sentence_assembler.py
@@ -1,23 +1,14 @@
 
-import json
+
+import ngram
+
 
 class sentence_assembler:
 
 	def __init__(self):
 
 		print "loading n-gram models..."
-		print "1 of 2"
-
-		f = open("datasets/ngram_backward.json", 'r')
-		self.ngram_backward = json.load(f)
-		f.close()
-
-		print "2 of 2"
-		'''
-		f = open("datasets/ngram_forward.json", 'r')
-		self.ngram_forward = json.load(f)
-		f.close()
-		'''
+		ngram.load()
 		
 
 	# make sentence using the supplied nouns and verbs
@@ -26,39 +17,4 @@ class sentence_assembler:
 		for token in tagged_tokens:
 			words.append(token[0])
 
-		return self.rateSentence(words)
-
-
-
-	def rateSentence(self, words):
-		rating = 0
-		prev1 = "<s>"
-		prev2 = "<s>"
-
-		# backward ngrams
-		for index, word in enumerate(words):
-
-			if index >= 1:
-				if self.ngram_backward.has_key(word):
-					if self.ngram_backward[word].has_key(prev1):
-						if self.ngram_backward[word][prev1].has_key(prev2):
-							rating += self.ngram_backward[word][prev1][prev2][0]
-			
-			prev2 = prev1
-			prev1 = word
-
-		return rating
-
-
-
-	def getNextWords(self, word):
-		if self.ngram_forward.has_key(word):
-			return self.ngram_forward[word].keys()
-		else:
-			return None
-
-	def getPrevWords(self, word):
-		if self.ngram_backward.has_key(word):
-			return self.ngram_backward[word].keys()
-		else:
-			return None
+		return ngram.rateSentence(words)
