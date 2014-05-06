f9d8832b4c596f659aff4142c04d186c15ffbcfe
Cody Smith <codysmithd@gmail.com>
2014-05-05 22:47:38 -0400

    Created corpus to relational script

---
+++ _generate_relational.py
@@ -0,0 +1,22 @@
+import json
+from relational import relational_map
+
+# Open corpus
+f = open("datasets/tagged_corpus.json", 'r')
+sentences = json.load(f)
+f.close()
+
+# Make new relational map
+long_term = relational_map()
+
+#n = 0
+
+for sentence in sentences:
+	#words_pos = []
+	for tuples in sentences:
+		print str(tuples[1]) + '\n'
+		#words_pos.append( (tuples[0],str(tuples[1])) )
+	#long_term.link_words(words_pos)
+	#print("\nSentence " + str(n) + " done\n")
+	#n += 1
+long_term.output_file("output.txt")
