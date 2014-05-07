584a27a9530aaaef39c9a74e1173aea3a1905868
Cody Smith <codysmithd@gmail.com>
2014-05-06 23:15:14 -0400

    Made testing return more results

--- _test_relational_map.py
+++ _test_relational_map.py
@@ -16,4 +16,4 @@ print("database loaded")
 while(True):
 	user_input = raw_input("\nWhat would you like to see links for?\n")
 	print("Top links for " + user_input.split()[0])
-	print(database.get_top_links_for_word((user_input.split()[0],1), 10) )
+	print(database.get_top_links_for_word((user_input.split()[0],1), 50) )
